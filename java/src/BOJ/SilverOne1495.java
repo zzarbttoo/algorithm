ed3430566703a237d52ecc27219bbbc07b199c9a
zzarbttoo <qodbwls70@naver.com>
2021-11-23 21:16:42 +0900

    1495

---
+++ java/src/BOJ/SilverOne1495.java
@@ -0,0 +1,68 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SilverOne1495 {
+
+    static int N, S, M;
+    static int[] V;
+    static int answer;
+    static Boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new Boolean[N + 1][1001];
+
+        V = new int[N];
+        answer = -1;
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N + 1; i ++){
+            Arrays.fill(visited[i], false);
+        }
+
+        for(int i = 0; i < N ; i ++){
+            V[i] =  Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, S);
+
+        System.out.println(answer);
+
+
+    }
+
+    public static void dfs(int idx, int before){
+
+        if (idx< N){
+            if (before + V[idx] <= M && before + V[idx] >= 0 && visited[idx + 1][before + V[idx]] == false){
+                dfs(idx + 1, before + V[idx]);
+                visited[idx + 1][before + V[idx]] = true;
+            }
+            if (before - V[idx] <= M && before - V[idx] >= 0 && visited[idx + 1][before - V[idx]] == false){
+                dfs(idx + 1, before - V[idx]);
+                visited[idx + 1][before - V[idx]] = true;
+            }
+
+        }else{
+            if (answer < before){
+                answer = before;
+            }
+        }
+
+    }
+
+
+}
